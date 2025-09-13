85e86a0e995eec32c2f8327a5d0d7116bcfacc90
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-09-14 01:01:37 +0800

    Bump version to 0.1.0

--- BangumiNet.Shared/ApiSetting.cs
+++ BangumiNet.Shared/ApiSetting.cs
@@ -2,6 +2,6 @@
 
 public class ApiSetting
 {
-    public string UserAgent { get; set; } = "ajtn123/BangumiNet/1.0";
+    public string UserAgent { get; set; } = "ajtn123/BangumiNet/0.1.0 (Windows) (https://github.com/ajtn123/BangumiNet)";
     public string AuthToken { get; set; } = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\BangumiDevToken");
 }
