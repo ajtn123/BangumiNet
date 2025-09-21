d2cb4fd246ebf642685c27d78a44a977fffe6171
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-09-21 14:00:37 +0800

    Fixes for darkmode and episode list

--- BangumiNet/Views/SubjectView.axaml.cs
+++ BangumiNet/Views/SubjectView.axaml.cs
@@ -13,14 +13,16 @@ public partial class SubjectView : ReactiveUserControl<SubjectViewModel>
         {
             if (dataContextChanges > 10) return;
             if (DataContext is not SubjectViewModel viewModel) return;
-            if (viewModel.Source?.GetType() == typeof(Api.V0.Models.Subject)) return;
-            if (viewModel.Id is not int id) return;
-            var fullSubject = await ApiC.V0.Subjects[id].GetAsync();
-            if (fullSubject == null) return;
-            dataContextChanges += 1;
-            var vm = new SubjectViewModel(fullSubject);
-            _ = vm.EpisodeListViewModel?.LoadEpisodes();
-            DataContext = vm;
+            if (viewModel.Source?.GetType() != typeof(Api.V0.Models.Subject))
+            {
+                if (viewModel.Id is not int id) return;
+                var fullSubject = await ApiC.V0.Subjects[id].GetAsync();
+                if (fullSubject == null) return;
+                dataContextChanges += 1;
+                var vm = new SubjectViewModel(fullSubject);
+                DataContext = vm;
+            }
+            _ = ViewModel?.EpisodeListViewModel?.LoadEpisodes();
         };
     }
 
