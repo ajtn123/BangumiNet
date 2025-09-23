8c1e3ca753435a1afeedb43d7d51eabc07e27971
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-09-23 18:07:12 +0800

    Style

--- BangumiNet/Converters/NameCnCvt.cs
+++ BangumiNet/Converters/NameCnCvt.cs
@@ -21,6 +21,26 @@ public class NameCnCvt : IValueConverter
         var name = nameProp?.GetValue(subject)?.ToString();
         var nameCn = nameCnProp?.GetValue(subject)?.ToString();
 
-        return !string.IsNullOrWhiteSpace(nameCn) && SettingProvider.CurrentSettings.PreferChineseNames ? nameCn : name;
+        return string.IsNullOrWhiteSpace(nameCn) ? name : SettingProvider.CurrentSettings.PreferChineseNames ? nameCn : name;
+    }
+}
+public class NameAltCvt : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => Convert(value);
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+
+    public static string? Convert(object? obj)
+    {
+        if (obj is not { } subject) return "Null";
+
+        var type = subject.GetType();
+        var nameProp = type.GetProperty("Name", BindingFlags.Public | BindingFlags.Instance);
+        var nameCnProp = type.GetProperty("NameCn", BindingFlags.Public | BindingFlags.Instance);
+        var name = nameProp?.GetValue(subject)?.ToString();
+        var nameCn = nameCnProp?.GetValue(subject)?.ToString();
+
+        return string.IsNullOrWhiteSpace(nameCn) ? null : SettingProvider.CurrentSettings.PreferChineseNames ? name : nameCn;
     }
 }
