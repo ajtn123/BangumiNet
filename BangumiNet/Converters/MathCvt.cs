0e2fdf37f9432e0e6fe924a7e3f51e9180b1cc0f
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-09-25 16:52:40 +0800

    Style

---
+++ BangumiNet/Converters/MathCvt.cs
@@ -0,0 +1,65 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace BangumiNet.Converters;
+
+public class AddCvt : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if ((double?)value is not double x) return null;
+        if ((double?)parameter is not double y) return x;
+        return x + y;
+    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if ((double?)value is not double x) return null;
+        if ((double?)parameter is not double y) return x;
+        return x - y;
+    }
+}
+public class SubCvt : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if ((double?)value is not double x) return null;
+        if ((double?)parameter is not double y) return x;
+        return x - y;
+    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if ((double?)value is not double x) return null;
+        if ((double?)parameter is not double y) return x;
+        return x + y;
+    }
+}
+public class MulCvt : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if ((double?)value is not double x) return null;
+        if ((double?)parameter is not double y) return x;
+        return x * y;
+    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if ((double?)value is not double x) return null;
+        if ((double?)parameter is not double y) return x;
+        return x / y;
+    }
+}
+public class DivCvt : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if ((double?)value is not double x) return null;
+        if ((double?)parameter is not double y) return x;
+        return x / y;
+    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if ((double?)value is not double x) return null;
+        if ((double?)parameter is not double y) return x;
+        return x * y;
+    }
+}
