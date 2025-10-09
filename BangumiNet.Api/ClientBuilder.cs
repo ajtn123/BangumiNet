a5b587d6bba347d3be186650c72e123a93ed0cda
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-10-09 21:35:21 +0800

    private client builder

--- BangumiNet.Api/ClientBuilder.cs
+++ BangumiNet.Api/ClientBuilder.cs
@@ -10,6 +10,7 @@ namespace BangumiNet.Api;
 
 public static class ClientBuilder
 {
+    public static P1.ApiClient GetP1Client(IApiSettings setting) => new(GetRequestAdapter(setting));
     public static V0.ApiClient GetV0Client(IApiSettings setting) => new(GetRequestAdapter(setting));
     public static Legacy.ApiClient GetLegacyClient(IApiSettings setting) => new(GetRequestAdapter(setting));
 
@@ -35,6 +36,7 @@ public static class ClientBuilder
         if (!string.IsNullOrWhiteSpace(setting.AuthToken))
             httpClient.DefaultRequestHeaders.Authorization = new("Bearer", setting.AuthToken);
         var requestAdapter = new HttpClientRequestAdapter(authProvider, httpClient: httpClient);
+        var p1Client = new P1.ApiClient(requestAdapter);
         var v0Client = new V0.ApiClient(requestAdapter);
         var legacyClient = new Legacy.ApiClient(requestAdapter);
         var htmlClient = new HtmlClient(setting, httpClient);
@@ -44,6 +46,7 @@ public static class ClientBuilder
             AuthenticationProvider = authProvider,
             HttpClient = httpClient,
             RequestAdapter = requestAdapter,
+            P1Client = p1Client,
             V0Client = v0Client,
             LegacyClient = legacyClient,
             HtmlClient = htmlClient
@@ -56,6 +59,7 @@ public class Clients
     public required IAuthenticationProvider AuthenticationProvider { get; set; }
     public required HttpClient HttpClient { get; set; }
     public required IRequestAdapter RequestAdapter { get; set; }
+    public required P1.ApiClient P1Client { get; set; }
     public required V0.ApiClient V0Client { get; set; }
     public required Legacy.ApiClient LegacyClient { get; set; }
     public required HtmlClient HtmlClient { get; set; }
