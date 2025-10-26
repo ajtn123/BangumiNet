10b7210aff2640128efccd371d1b89243653f1a5
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-10-26 20:32:44 +0800

    fix bbcode quote

--- BangumiNet/Utils/BBCodeHelper.cs
+++ BangumiNet/Utils/BBCodeHelper.cs
@@ -8,12 +8,17 @@ namespace BangumiNet.Utils;
 
 public static partial class BBCodeHelper
 {
+    public const string ContentPos = "%CONTENT%";
+    public const string SelectionBackgroundPos = "%SELECTIONBACKGROUND%";
     public static void Parser(HtmlPanel hp, AvaloniaPropertyChangedEventArgs e)
     {
-        var html = ParseBBCode(hp.Tag?.ToString());
+        var bbcode = hp.Tag?.ToString();
+        if (string.IsNullOrWhiteSpace(bbcode)) return;
+
+        var html = ParseBBCode(bbcode);
         object? brush = null;
         Application.Current?.TryGetResource("SystemFillColorAttentionBrush", out brush);
-        html = html.Replace("%SelectionBackground%", (brush as SolidColorBrush)?.Color.ToString().Replace("#ff", "#") ?? "#fff");
+        html = html.Replace(SelectionBackgroundPos, (brush as SolidColorBrush)?.Color.ToString().Replace("#ff", "#") ?? "#fff");
         hp.Text = html;
     }
 
@@ -21,14 +26,14 @@ public static partial class BBCodeHelper
     {
         if (string.IsNullOrEmpty(bbcode)) return string.Empty;
 
-        string result = bbcode.Trim(' ', '\n');
+        string result = bbcode.Trim(' ', '\n', '\r');
         foreach (var p in BBCodeRegexReplacement) result = p.Key.Replace(result, p.Value);
         foreach (var p in BBCodeReplacement) result = result.Replace(p.Key, p.Value);
 
-        return Html.Replace("{content}", result);
+        return Html.Replace(ContentPos, result);
     }
 
-    private static readonly string Html = """
+    private static readonly string Html = $$"""
         <!DOCTYPE html>
         <html>
         <head>
@@ -36,14 +41,17 @@ public static partial class BBCodeHelper
             html { margin: 0; padding: 0; }
             body { margin: 0; padding: 0; word-break: break-all; }
             span.mask { background-color: #000; color: #000; corner-radius: 5px; }
-            ::selection { background-color: %SelectionBackground%; color: #000; }
+            blockquote { margin: 0.5em; color: #777; }
+            ::selection { background-color: {{SelectionBackgroundPos}}; color: #000; }
         </style>
         </head>
-        <body>{content}</body>
+        <body>{{ContentPos}}</body>
         </html>
         """;
     private static readonly Dictionary<string, string> BBCodeReplacement = new()
     {
+        ["\r\n"] = "<br/>",
+        ["\n"] = "<br/>",
         ["[b]"] = "<b>",
         ["[/b]"] = "</b>",
         ["[i]"] = "<i>",
@@ -54,6 +62,10 @@ public static partial class BBCodeHelper
         ["[/s]"] = "</s>",
         ["[mask]"] = "<span class=\"mask\">",
         ["[/mask]"] = "</span>",
+        ["<br/>[quote]"] = "[quote]",
+        ["[/quote]<br/>"] = "[/quote]",
+        ["[quote]"] = "<blockquote>",
+        ["[/quote]"] = "</blockquote>",
         ["[/color]"] = "</span>",
         ["[/size]"] = "</span>",
         ["[/url]"] = "</a>",
@@ -64,7 +76,7 @@ public static partial class BBCodeHelper
         [SizeOpen()] = "<span style=\"font-size: $1px;\">",
         [LinkLiteral()] = "<a href=\"$1\">$1</a>",
         [LinkOpen()] = "<a href=\"$1\">",
-        [Image()] = "<img src=\"$1\"></img>",
+        [Image()] = "<img src=\"$1\"/>",
     };
 
     [GeneratedRegex(@"\[color=([#0-9a-zA-Z]*)\]")]
