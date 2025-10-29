615ddcdfc4a3a7040e82398960c2487d96fdd4f3
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-10-29 23:41:16 +0800

    update api info

--- BangumiNet.Api/Extensions.cs
+++ BangumiNet.Api/Extensions.cs
@@ -16,9 +16,9 @@ namespace BangumiNet.Api.V0
     public partial class ApiClient : IApiClient
     {
         /// <summary>OpenAPI 定义文件的最后 commit</summary>
-        public const string Commit = "4433d6a0265e23a12324180569ac4abc964e682b";
+        public const string Commit = "117fc456a45c1e2a5a0647d3d3cf0e4e74a28344";
         /// <summary>OpenAPI 定义文件的 URL</summary>
-        public const string DefinitionUrl = "https://raw.githubusercontent.com/bangumi/api/refs/heads/master/open-api/v0.yaml";
+        public const string DefinitionUrl = "https://github.com/trim21-bot/api/raw/117fc456a45c1e2a5a0647d3d3cf0e4e74a28344/open-api/v0.yaml";
         /// <summary>生成本 API 客户端的命令</summary>
         public const string KiotaCommand = $@"kiota generate -d {DefinitionUrl} -l csharp -n BangumiNet.Api.V0 -o .\V0 --co";
     }
