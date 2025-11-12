cbe6cd64a27440cd30eb128eec77b9d360131689
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-11-12 19:22:39 +0800

    highlight collected subjects in airing view

--- BangumiNet/ViewModels/AiringViewModel.cs
+++ BangumiNet/ViewModels/AiringViewModel.cs
@@ -1,4 +1,5 @@
-﻿using BangumiNet.Api.P1.Models;
+﻿using BangumiNet.Api.ExtraEnums;
+using BangumiNet.Api.P1.Models;
 
 namespace BangumiNet.ViewModels;
 
@@ -12,6 +13,15 @@ public partial class AiringViewModel : ViewModelBase
     public AiringViewModel(Calendar calendar)
     {
         Calendars = [.. calendar.Days.Select(calendar => new CalendarViewModel(calendar))];
+        Task.Run(async () =>
+        {
+            var cl = new SubjectCollectionListViewModel(ItemType.Subject, SubjectType.Anime, CollectionType.Doing);
+            await cl.Load();
+            var cids = cl.SubjectList.SubjectViewModels?.Select(s => (s as SubjectCollectionViewModel)?.Subject?.Id).ToArray();
+            if (cids == null) return;
+            foreach (var subject in Calendars.SelectMany(x => x.Subjects ?? []))
+                if (cids.Contains(subject.Id)) subject.IsEmphasized = true;
+        });
     }
 
     [Reactive] public partial ObservableCollection<CalendarViewModel>? Calendars { get; set; }
--- BangumiNet/ViewModels/ViewModelBase.cs
+++ BangumiNet/ViewModels/ViewModelBase.cs
@@ -6,8 +6,10 @@ public partial class ViewModelBase : ReactiveObject
     {
         Title = Shared.Constants.ApplicationName;
         IsVisible = true;
+        IsEmphasized = false;
     }
     public static Settings CurrentSettings => SettingProvider.CurrentSettings;
     [Reactive] public partial string Title { get; set; }
     [Reactive] public partial bool IsVisible { get; set; }
+    [Reactive] public partial bool IsEmphasized { get; set; }
 }
