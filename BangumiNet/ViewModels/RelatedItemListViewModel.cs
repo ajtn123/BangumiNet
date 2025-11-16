0daa00196e736eb2bf55bdf27de2dfbc307af77e
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-11-17 00:37:52 +0800

    related items

--- BangumiNet/ViewModels/RelatedItemListViewModel.cs
+++ BangumiNet/ViewModels/RelatedItemListViewModel.cs
@@ -11,63 +11,51 @@ public partial class RelatedItemListViewModel : SubjectListViewModel
         ParentId = parentId;
     }
 
-    public Task Load(CancellationToken cancellationToken = default) => Type switch
+    public async Task Load(CancellationToken cancellationToken = default)
     {
-        ItemType.Subject => LoadSubjects(cancellationToken),
-        ItemType.Character => LoadCharacters(cancellationToken),
-        ItemType.Person => LoadPersons(cancellationToken),
+        if (ParentId is not int id) return;
+        IEnumerable<object>? response = null;
+        try
+        {
+            response = Type switch
+            {
+                ItemType.Subject => ParentType switch
+                {
+                    ItemType.Subject => await ApiC.V0.Subjects[id].Subjects.GetAsync(cancellationToken: cancellationToken),
+                    ItemType.Character => await ApiC.V0.Characters[id].Subjects.GetAsync(cancellationToken: cancellationToken),
+                    ItemType.Person => await ApiC.V0.Persons[id].Subjects.GetAsync(cancellationToken: cancellationToken),
+                    _ => throw new NotImplementedException(),
+                },
+                ItemType.Character => ParentType switch
+                {
+                    ItemType.Subject => await ApiC.V0.Subjects[id].Characters.GetAsync(cancellationToken: cancellationToken),
+                    ItemType.Person => await ApiC.V0.Persons[id].Characters.GetAsync(cancellationToken: cancellationToken),
+                    _ => throw new NotImplementedException(),
+                },
+                ItemType.Person => ParentType switch
+                {
+                    ItemType.Subject => await ApiC.V0.Subjects[id].Persons.GetAsync(cancellationToken: cancellationToken),
+                    ItemType.Character => await ApiC.V0.Characters[id].Persons.GetAsync(cancellationToken: cancellationToken),
+                    _ => throw new NotImplementedException(),
+                },
+                _ => throw new NotImplementedException(),
+            };
+        }
+        catch (Exception e) { Trace.TraceError(e.Message); }
+        if (response == null) return;
+        SubjectViewModels = [.. response.Select(ConvertToVM)];
+    }
+
+    private static ViewModelBase ConvertToVM(object obj) => obj switch
+    {
+        V0_subject_relation vsr => new SubjectViewModel(vsr),
+        V0_RelatedSubject vrs => new SubjectViewModel(vrs),
+        RelatedCharacter rc => new CharacterViewModel(rc),
+        PersonCharacter pc => new CharacterViewModel(pc),
+        RelatedPerson rp => new PersonViewModel(rp),
+        CharacterPerson cp => new PersonViewModel(cp),
         _ => throw new NotImplementedException(),
     };
-
-    private async Task LoadSubjects(CancellationToken cancellationToken = default)
-    {
-        if (ParentId is not int id) return;
-        if (ParentType == ItemType.Subject)
-        {
-            var data = await ApiC.V0.Subjects[id].Subjects.GetAsync(cancellationToken: cancellationToken);
-            SubjectViewModels = data?.Select<V0_subject_relation, ViewModelBase>(x => new SubjectViewModel(x)).ToObservableCollection();
-        }
-        else if (ParentType == ItemType.Character)
-        {
-            var data = await ApiC.V0.Characters[id].Subjects.GetAsync(cancellationToken: cancellationToken);
-            SubjectViewModels = data?.Select<V0_RelatedSubject, ViewModelBase>(x => new SubjectViewModel(x)).ToObservableCollection();
-        }
-        else if (ParentType == ItemType.Person)
-        {
-            var data = await ApiC.V0.Persons[id].Subjects.GetAsync(cancellationToken: cancellationToken);
-            SubjectViewModels = data?.Select<V0_RelatedSubject, ViewModelBase>(x => new SubjectViewModel(x)).ToObservableCollection();
-        }
-    }
-
-    private async Task LoadCharacters(CancellationToken cancellationToken = default)
-    {
-        if (ParentId is not int id) return;
-        if (ParentType == ItemType.Subject)
-        {
-            var data = await ApiC.V0.Subjects[id].Characters.GetAsync(cancellationToken: cancellationToken);
-            SubjectViewModels = data?.Select<RelatedCharacter, ViewModelBase>(x => new CharacterViewModel(x)).ToObservableCollection();
-        }
-        else if (ParentType == ItemType.Person)
-        {
-            var data = await ApiC.V0.Persons[id].Characters.GetAsync(cancellationToken: cancellationToken);
-            SubjectViewModels = data?.Select<PersonCharacter, ViewModelBase>(x => new CharacterViewModel(x)).ToObservableCollection();
-        }
-    }
-
-    private async Task LoadPersons(CancellationToken cancellationToken = default)
-    {
-        if (ParentId is not int id) return;
-        if (ParentType == ItemType.Subject)
-        {
-            var data = await ApiC.V0.Subjects[id].Persons.GetAsync(cancellationToken: cancellationToken);
-            SubjectViewModels = data?.Select<RelatedPerson, ViewModelBase>(x => new PersonViewModel(x)).ToObservableCollection();
-        }
-        else if (ParentType == ItemType.Character)
-        {
-            var data = await ApiC.V0.Characters[id].Persons.GetAsync(cancellationToken: cancellationToken);
-            SubjectViewModels = data?.Select<CharacterPerson, ViewModelBase>(x => new PersonViewModel(x)).ToObservableCollection();
-        }
-    }
 
     [Reactive] public partial ItemType Type { get; set; }
     [Reactive] public partial ItemType ParentType { get; set; }
