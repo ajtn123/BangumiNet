76276c26024156f88c789b25c0c05dea700ead1a
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-11-22 01:44:17 +0800

    collection count

---
+++ BangumiNet/Utils/LayoutUtils.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.TextFormatting;
+
+namespace BangumiNet.Utils;
+
+public class LayoutUtils
+{
+    // Source - https://stackoverflow.com/a/78945115
+    // Posted by cboittin, modified by community. See post 'Timeline' for change history
+    // Retrieved 2025-11-22, License - CC BY-SA 4.0
+    public static Size CalculateTextSize(string? text, double fontSize, FontFamily? fontFamily = null)
+    {
+        if (string.IsNullOrEmpty(text)) text = " ";
+        fontFamily ??= new("Microsoft YaHei UI");
+        var ts = TextShaper.Current;
+        var typeface = new Typeface(fontFamily);
+        var shaped = ts.ShapeText(text, new TextShaperOptions(typeface.GlyphTypeface, fontSize));
+        var run = new ShapedTextRun(shaped, new GenericTextRunProperties(typeface, fontSize));
+        return run.Size;
+    }
+}
---
+++ BangumiNet/Views/SubjectCollectionCountView.axaml.cs
@@ -0,0 +1,50 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Documents;
+using BangumiNet.Api.Interfaces;
+using FluentAvalonia.Core;
+using System.Reactive.Linq;
+
+namespace BangumiNet.Views;
+
+public partial class SubjectCollectionCountView : UserControl
+{
+    public SubjectCollectionCountView()
+    {
+        InitializeComponent();
+        this.WhenAnyValue(x => x.DataContext).Subscribe(dc =>
+        {
+            if (dc is not ICollection col || col.GetTotal() == 0)
+            {
+                LayoutGird.ColumnDefinitions = [GetCD(1), GetCD(1), GetCD(1), GetCD(1), GetCD(1)];
+                WishRun.Text = DoingRun.Text = DoneRun.Text = OnHoldRun.Text = DroppedRun.Text = string.Empty;
+                return;
+            }
+
+            WishRun.Text = col.Wish.ToString();
+            DoingRun.Text = col.Doing.ToString();
+            DoneRun.Text = col.Collect.ToString();
+            OnHoldRun.Text = col.OnHold.ToString();
+            DroppedRun.Text = col.Dropped.ToString();
+
+            LayoutGird.ColumnDefinitions = [
+                GetCD(col.Wish, WishRun),
+                GetCD(col.Doing, DoingRun),
+                GetCD(col.Collect, DoneRun),
+                GetCD(col.OnHold, OnHoldRun),
+                GetCD(col.Dropped, DroppedRun),
+            ];
+
+            ColumnDefinition GetCD(int? width, Run? run = null)
+            {
+                ColumnDefinition cd;
+                if (width is int w)
+                    cd = new(w, GridUnitType.Star);
+                else
+                    cd = new(0, GridUnitType.Auto);
+                if (run != null)
+                    cd.Bind(ColumnDefinition.MinWidthProperty, this.WhenAnyValue(x => x.IsPointerOver).Select(isPointerOver => isPointerOver ? LayoutUtils.CalculateTextSize("占位 " + run.Text, 14, run.FontFamily).Width + 20 : 5d));
+                return cd;
+            }
+        });
+    }
+}
