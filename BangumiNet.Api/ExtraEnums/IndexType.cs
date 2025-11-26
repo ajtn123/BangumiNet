658e87dac31030ad385c4713fa1a3ec5f1956ee7
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-11-27 00:01:17 +0800

    report & index types

---
+++ BangumiNet.Api/ExtraEnums/IndexType.cs
@@ -0,0 +1,27 @@
+﻿namespace BangumiNet.Api.ExtraEnums;
+// https://github.com/bangumi/server-private/blob/master/lib/index/types.ts
+// https://github.com/bangumi/server-private/blob/master/lib/types/common.ts
+public enum IndexType
+{
+    User = 0,
+    Public = 1,
+    Award = 2,
+}
+
+public enum IndexPrivacy
+{
+    Normal = 0,
+    Ban = 1,
+    Private = 2,
+}
+
+public enum IndexRelatedCategory
+{
+    Subject = 0,
+    Character = 1,
+    Person = 2,
+    Episode = 3,
+    Blog = 4,
+    GroupTopic = 5,
+    SubjectTopic = 6,
+}
---
+++ BangumiNet.Api/ExtraEnums/ReportType.cs
@@ -0,0 +1,33 @@
+﻿namespace BangumiNet.Api.ExtraEnums;
+
+public enum ReportType
+{
+    User = 6,
+    GroupTopic = 7,
+    GroupReply = 8,
+    SubjectTopic = 9,
+    SubjectReply = 10,
+    EpisodeReply = 11,
+    CharacterReply = 12,
+    PersonReply = 13,
+    Blog = 14,
+    BlogReply = 15,
+    Timeline = 16,
+    TimelineReply = 17,
+    Index = 18,
+    IndexReply = 19,
+}
+
+public enum ReportReason
+{
+    Abuse = 1,
+    Spam = 2,
+    Political = 3,
+    Illegal = 4,
+    Privacy = 5,
+    CheatScore = 6,
+    Flame = 7,
+    Advertisement = 8,
+    Spoiler = 9,
+    Other = 99,
+}
