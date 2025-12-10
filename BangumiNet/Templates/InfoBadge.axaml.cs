1ead9a47de529ffd3c960fd7c5727dad24009b90
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-12-10 17:19:16 +0800

    index stats

---
+++ BangumiNet/Templates/InfoBadge.axaml.cs
@@ -0,0 +1,28 @@
+using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace BangumiNet.Templates;
+
+public class InfoBadge : TemplatedControl
+{
+    public InfoBadge()
+    {
+        this.WhenAnyValue(x => x.Text).Subscribe(t => IsVisible = !string.IsNullOrWhiteSpace(t));
+    }
+
+    public static readonly StyledProperty<string?> LabelProperty
+        = AvaloniaProperty.Register<InfoBadge, string?>(nameof(Label));
+    public string? Label
+    {
+        get => GetValue(LabelProperty);
+        set => SetValue(LabelProperty, value);
+    }
+
+    public static readonly StyledProperty<string?> TextProperty
+        = AvaloniaProperty.Register<InfoBadge, string?>(nameof(Text));
+    public string? Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
---
+++ BangumiNet/Views/IndexStatsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BangumiNet.Views;
+
+public partial class IndexStatsView : UserControl
+{
+    public IndexStatsView()
+    {
+        InitializeComponent();
+    }
+}
