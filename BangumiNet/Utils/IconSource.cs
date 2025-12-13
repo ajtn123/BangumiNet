43166f0fb46025641b3f67c809cb275a564ffd08
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-12-13 16:35:25 +0800

    iconsource markup

---
+++ BangumiNet/Utils/IconSource.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Markup.Xaml;
+using FluentAvalonia.UI.Controls;
+using FluentIcons.Avalonia;
+using FluentIcons.Common;
+
+namespace BangumiNet.Utils;
+
+public class IconSource(Icon icon) : MarkupExtension
+{
+    private readonly Icon icon = icon;
+    public override ImageIconSource ProvideValue(IServiceProvider provider)
+        => new() { Source = new FluentImage { Icon = icon } };
+}
