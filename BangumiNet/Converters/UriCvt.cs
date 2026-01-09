d23bb797ea3a3eae7045267fac46801367c1b2d4
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-01-09 10:47:37 +0800

    fix flyout maxwidth

---
+++ BangumiNet/Converters/UriCvt.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace BangumiNet.Converters;
+
+public class UriCvt : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is string str && !str.IsWhiteSpace() ? new Uri(str) : null;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => (value as Uri)?.ToString();
+}
