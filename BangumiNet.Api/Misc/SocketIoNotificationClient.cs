d96cd661d2242deaf62e281643e8c7a3550008b4
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-03-02 18:38:20 +0800

    deps: SocketIoClient 4.0.1 migration

--- BangumiNet.Api/Misc/SocketIoNotificationClient.cs
+++ BangumiNet.Api/Misc/SocketIoNotificationClient.cs
@@ -5,33 +5,27 @@ public class SocketIoNotificationClient : IDisposable
 {
     public record NotifyEvent(int Count);
 
-    public const string Host = "https://next.bgm.tv/";
+    public static readonly Uri Host = new("https://next.bgm.tv/");
     public const string Path = "/p1/socket-io/";
     public const string NotifyEventName = "notify";
     public const string ErrorEventName = "connect_error";
 
-    public SocketIoNotificationClient(IApiSettings settings, Action<NotifyEvent> callback)
+    public SocketIoNotificationClient(IApiSettings settings, Func<NotifyEvent?, Task> callback)
     {
         Client = GetClient(settings);
-        Client.On(NotifyEventName, r => callback(r.GetValue<NotifyEvent>()));
-    }
-    public SocketIoNotificationClient(IApiSettings settings, Func<NotifyEvent, Task> callback)
-    {
-        Client = GetClient(settings);
-        Client.On(NotifyEventName, r => callback(r.GetValue<NotifyEvent>()));
+        Client.On(NotifyEventName, response => callback(response.GetValue<NotifyEvent>(0)));
     }
 
     private static SocketIOClient.SocketIO GetClient(IApiSettings settings)
     {
-        var client = new SocketIOClient.SocketIO(Host, new()
+        var client = new SocketIOClient.SocketIO(Host, new SocketIOClient.SocketIOOptions()
         {
             Path = Path,
             Reconnection = true,
-            ReconnectionDelay = 5000,
             ReconnectionDelayMax = 10000,
-            Transport = SocketIOClient.Transport.TransportProtocol.WebSocket,
+            Transport = SocketIOClient.Common.TransportProtocol.WebSocket,
         });
-        client.Options.ExtraHeaders = new()
+        client.Options.ExtraHeaders = new Dictionary<string, string>()
         {
             ["Cookie"] = $"chiiNextSessionID={settings.AuthToken}",
             ["User-Agent"] = settings.UserAgent,
@@ -39,7 +33,9 @@ public class SocketIoNotificationClient : IDisposable
         client.On(ErrorEventName, response =>
         {
             client.Options.AutoUpgrade = true;
-            client.Options.Transport = SocketIOClient.Transport.TransportProtocol.Polling;
+            client.Options.Transport = SocketIOClient.Common.TransportProtocol.Polling;
+            System.Diagnostics.Trace.TraceWarning($"Bangumi socket.io notification emitted an error, downgrading transport protocol.");
+            return Task.CompletedTask;
         });
         return client;
     }
