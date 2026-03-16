a329357df8d863b6e189d2dc22f9cafff9cab495
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-03-16 16:50:55 +0800

    feat: add AuthenticationMethod enum and referer method

--- BangumiNet.Api/BangumiAuthenticationProvider.cs
+++ BangumiNet.Api/BangumiAuthenticationProvider.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.Kiota.Abstractions;
 using Microsoft.Kiota.Abstractions.Authentication;
+using static BangumiNet.Api.BangumiAuthenticationProvider;
 
 namespace BangumiNet.Api;
 
-public class BangumiAuthenticationProvider(string? token) : IAuthenticationProvider
+public class BangumiAuthenticationProvider(string? token, AuthenticationMethod methods = AuthenticationMethod.All) : IAuthenticationProvider
 {
-    private readonly string? token = token;
     public Task AuthenticateRequestAsync(RequestInformation request, Dictionary<string, object>? additionalAuthenticationContext = null, CancellationToken cancellationToken = default)
     {
-        if (string.IsNullOrWhiteSpace(token)) return Task.CompletedTask;
+        if (methods.HasFlag(AuthenticationMethod.Referer))
+            request.Headers.Add("Referer", P1.ApiClient.BaseUrl);
 
-        request.Headers.Add("Authorization", $"Bearer {token}");
-        request.Headers.Add("Cookie", $"chiiNextSessionID={token}");
+        if (!string.IsNullOrWhiteSpace(token))
+        {
+            if (methods.HasFlag(AuthenticationMethod.Bearer))
+                request.Headers.Add("Authorization", $"Bearer {token}");
+
+            if (methods.HasFlag(AuthenticationMethod.Cookie))
+                request.Headers.Add("Cookie", $"chiiNextSessionID={token}");
+        }
+
         return Task.CompletedTask;
+    }
+
+    [Flags]
+    public enum AuthenticationMethod
+    {
+        None = 0,
+
+        Bearer = 1 << 0,
+        Cookie = 1 << 1,
+
+        Referer = 1 << 2,
+
+        All = Bearer | Cookie | Referer,
     }
 }
