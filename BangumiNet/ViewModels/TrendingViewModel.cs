46c4b25f044d1ad77a15b4ac72c6d145b7f8e006
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-08-26 15:32:01 +0800

    fix: ex

--- BangumiNet/ViewModels/TrendingViewModel.cs
+++ BangumiNet/ViewModels/TrendingViewModel.cs
@@ -66,7 +66,7 @@ public partial class TrendingViewModel : SubjectListPagedViewModel
             if (response == null) return;
 
             SubjectViewModels = response.Data?
-                .Select<ChannelSubjectTopic, ViewModelBase>(s => new TopicViewModel(s, false))
+                .Select<SubjectTopic, ViewModelBase>(s => new TopicViewModel(s, false))
                 .ToObservableCollection();
             PageNavigator.UpdatePageInfo(Limit, offset, response.Total);
         }
